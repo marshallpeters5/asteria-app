d2a3c1e5c94d029bf778c4b825a90320e2a7a7c4
Marshall <marshall.peters01@gmail.com>
2023-03-30 21:26:23 -0400

    started work on event listener for the user to submit their selected date

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,22 @@
 var key = '0qMWKgY4Hc74rbHIH8PQajMGaFPK4oztpyJCkqS4';
-var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2022-5-11&api_key='
+var selectedDate = document.getElementById('input-date')
+var url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=' + selectedDate + '&page=1&api_key='
 
-fetch(url + key)
+function getApi(event){
+fetch(url + key, {
+    earth_date: selectedDate,
+})
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log(data)
     });
+}
+
+function submitDateEventListener(){
+    const submitDate = document.getElementById('submit')
+    submitDate.addEventListener("submit", getApi)
+    console.log(submitDate)
+}
+console.log(earth_date)
